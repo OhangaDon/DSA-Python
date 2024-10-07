6e36c1750f759175f87dbcaed0e073cd472a2a46
OhangaDon <ochieng.vincent2022@students.jkuat.ac.ke>
2024-10-07 06:04:01 -0400

    commit

---
+++ Hash.py
@@ -0,0 +1,84 @@
+class HashTable:
+    def insert(self,key,value):
+        pass
+
+    def find(self,key):
+        pass
+
+    def update(self,key,value):
+        pass
+
+    def list_all(self):
+        pass
+
+MAX_HASH_TABLE_SIZE = 4096
+
+data_list = [None]*MAX_HASH_TABLE_SIZE
+
+def get_index(data_list, a_string):
+
+    result = 0
+
+    for a_character in a_string:
+        a_number = ord(a_character)
+        result += a_number
+
+    list_index = result % len(data_list)
+
+    return list_index
+
+key,value = 'Aakash', '7878787878'
+idx = get_index(data_list,key)
+data_list[idx] = (key,value)
+data_list[get_index(data_list, 'Hemanth')] = ('Hemanth', '9595949494')
+pairs = [kv[0] for kv in data_list if kv is not None]
+
+class BasicHashTable:
+    def __init__(self, max_size=MAX_HASH_TABLE_SIZE):
+        # 1. Create a list of size `max_size` with all values None
+        self.data_list = [None]*max_size
+     
+    
+    def insert(self, key, value):
+        # 1. Find the index for the key using get_index
+        idx = get_index(self.data_list, key)
+        
+        # 2. Store the key-value pair at the right index
+        self.data_list[idx] = key,value
+    
+    
+    def find(self, key):
+        # 1. Find the index for the key using get_index
+        idx = get_index(self.data_list,key)
+        
+        # 2. Retrieve the data stored at the index
+        kv = data_list[idx]
+        
+        # 3. Return the value if found, else return None
+        if kv is None:
+            return None
+        else:
+            key, value = kv
+            return value
+    
+    
+    def update(self, key, value):
+        # 1. Find the index for the key using get_index
+        idx = get_index(self.data_list,key)
+        
+        # 2. Store the new key-value pair at the right index
+        self.data_list[idx] = value
+
+    
+    def list_all(self):
+        # 1. Extract the key from each key-value pair 
+        return [kv[0] for kv in self.data_list if kv is not None]
+    
+basic_table = BasicHashTable(max_size=1024)
+# Insert some values
+basic_table.insert('Aakash', '9999999999')
+basic_table.insert('Hemanth', '8888888888')
+
+# Find a value
+basic_table.find('Hemanth') == '8888888888'
+print(basic_table.list_all())
